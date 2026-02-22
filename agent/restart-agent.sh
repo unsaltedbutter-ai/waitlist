007db3875a737b8c6716309a9951e2b9084d4283
Info <info@unsaltedbutter.ai>
2026-02-22 15:12:37 -0800

    tweak restart scripts

--- agent/restart-agent.sh
+++ agent/restart-agent.sh
@@ -1,3 +1,6 @@
+echo "launchctl kickstart -k gui/501/com.unsaltedbutter.agent"
+echo "tail -f ~/logs/agent-stderr.log"
+
 launchctl kickstart -k gui/501/com.unsaltedbutter.agent
 
 
--- orchestrator/restart-orchestrator.sh
+++ orchestrator/restart-orchestrator.sh
@@ -1,3 +1,6 @@
+echo "launchctl kickstart -k gui/501/com.unsaltedbutter.orchestrator"
+echo "tail -f ~/logs/orchestrator-stderr.log"
+
 launchctl kickstart -k gui/501/com.unsaltedbutter.orchestrator
 
 tail -f ~/logs/orchestrator-stderr.log
