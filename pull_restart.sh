0858bfb7104cf55247e6f19d18f151d5641f1178
Info <info@unsaltedbutter.ai>
2026-02-22 19:33:22 -0800

    tweak script

--- pull_restart.sh
+++ pull_restart.sh
@@ -1,9 +1,9 @@
 echo "git pull";
 echo "launchctl kickstart -k gui/501/com.unsaltedbutter.orchestrator"
 echo "launchctl kickstart -k gui/501/com.unsaltedbutter.agent"
-echo "======================================"
-echo "======$(git rev-parse --short HEAD) ======"
-echo "======================================"
+echo ="======================================"
+echo "=============   $(git rev-parse --short HEAD)   ============="
+echo "======================================="
 
 git pull;
 launchctl kickstart -k gui/501/com.unsaltedbutter.orchestrator;
