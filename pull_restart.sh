8c42f223105702291ef866b005ea0d37bcbad6bb
Info <info@unsaltedbutter.ai>
2026-02-22 20:28:56 -0800

    fixup script

--- pull_restart.sh
+++ pull_restart.sh
@@ -1,10 +1,14 @@
 echo "git pull";
 echo "launchctl kickstart -k gui/501/com.unsaltedbutter.orchestrator"
 echo "launchctl kickstart -k gui/501/com.unsaltedbutter.agent"
+
+echo "======================================"
+echo "               git pull"
+echo "======================================"
+git pull;
 echo ="======================================"
 echo "=============   $(git rev-parse --short HEAD)   ============="
 echo "======================================="
 
-git pull;
 launchctl kickstart -k gui/501/com.unsaltedbutter.orchestrator;
 launchctl kickstart -k gui/501/com.unsaltedbutter.agent;
